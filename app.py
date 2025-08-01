9663c0772beb0b13b111bcac0b75ac707241d2d9
anonymouss9xii-art <anonymouss.9xii@gmail.com>
2025-08-01 23:22:19 +0300

    Fix port for Railway

--- app.py
+++ app.py
@@ -256,4 +256,4 @@ def submit():
     return render_template('result.html', score=score, total=len(questions))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
